847fb660370f12507775f635c2edd185f44d9124
saichann <55883274+sai11121209@users.noreply.github.com>
2022-06-04 15:52:39 +0000

    修正

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 pip install -U discord.py
-pip install -U discord-py-slash-command
+pip3 install aiohttp==3.7.4
+pip install -U discord-py-slash-command==3.0.3
 pip install requests
 pip install beautifulsoup4
 pip install lxml
